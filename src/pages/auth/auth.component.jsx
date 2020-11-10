f7fb8ef3470e15d9ccbd7d3d426f26d3a24001d7
Isaac Vázquez <isaacv2514@gmail.com>
2020-11-10 08:51:15 -0600

    Github pages implemente

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import background from '../../assets/images/auth.png';
+import {ReactComponent as Logo} from '../../assets/svg/logo.svg';
 
 import {
     SignIn
 } from '../../components';
 
 const AuthPage = () => (
-    <div className="min-w-screen min-h-screen bg-blue-700 flex flex-col justify-center items-center text-white px-4 md:px-0">
+    <div style={{ background: `url('${background}')` }} className="min-w-screen min-h-screen bg-blue-700 flex flex-col justify-center items-center text-white px-4 md:px-0">
         <div className="w-full md:w-2/3">
-            <h1 className="text-center text-xl">Sistema de Administración de laminillas y bloques.</h1>
+            <div className="flex flex-col items-center">
+                <Logo />
+                <h1 className="text-center text-xl mt-3">Sistema de Administración de laminillas y bloques.</h1>
+            </div>
 
             <h4 className="text-3xl font-bold mt-12">Inicia sesión</h4>
             <p className="mb-8 font-thin">Ingresa tus datos correctamente para poder entrar.</p>
