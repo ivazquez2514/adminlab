6674231cd09e43952d5dc7efe4e61306b8ad69a3
Isaac Vázquez <isaacv2514@gmail.com>
2020-11-20 04:49:23 -0600

    Change api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://45.79.22.211/graphql',
+  uri: 'https://adminlab.tk/graphql',
   cache: new InMemoryCache()
 });
 
