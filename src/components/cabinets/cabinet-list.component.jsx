5925d2a5401fdfbe9a998afc47b788cc502f93bd
Isaac Vázquez <isaacv2514@gmail.com>
2020-12-08 17:45:11 -0600

    Add search by case number in cabinets list

--- src/components/cabinets/cabinet-list.component.jsx
+++ src/components/cabinets/cabinet-list.component.jsx
@@ -19,16 +19,27 @@ const CabinetList = ({history, setCabinet, search, setSearch}) => {
         list = [...data.cabinetList];
     }
 
+    const getTotalItemsCount = (type) => {
+        return list
+            .filter(item => type === item.cabinetType)
+            .filter(item =>
+                String(item.cabinetNumber).toLowerCase().includes(search.toLowerCase())
+                || String(item.expedients.length).includes(search.toLowerCase())
+                || item.expedients.filter(expedient => expedient.caseNumber.toLowerCase().includes(search.toLowerCase())).length > 0)
+            .length;
+    };
+
     const getItems = useCallback(() => {
         return list.filter(item => (listType === 'BLOCKS' ? 'Bloques' : 'Laminillas') === item.cabinetType)
             .splice(offset, 3);
     }, [listType, data, offset]);
 
-    const filteredItems = useCallback(() => {
+    const filteredItems = useCallback((type) => {
         if (search) {
             return getItems().filter(item =>
                 String(item.cabinetNumber).toLowerCase().includes(search.toLowerCase())
-                || String(item.expedients.length).includes(search.toLowerCase()));
+                || String(item.expedients.length).includes(search.toLowerCase())
+                || item.expedients.filter(expedient => expedient.caseNumber.toLowerCase().includes(search.toLowerCase())).length > 0);
         }
 
         return getItems();
@@ -41,7 +52,7 @@ const CabinetList = ({history, setCabinet, search, setSearch}) => {
     }, []);
 
     return (
-        <div className="relative h-full md:flex flex-col items-center justify-center list-container" style={{height: 'calc(100vh - 200px)'}}>
+        <div className="relative h-full md:flex flex-col items-center justify-center list-container overflow-y-auto" style={{height: 'calc(100vh - 200px)'}}>
                 <div className="w-full bg-gray-400 p-1 rounded-lg font-bold text-gray-500 text-lg mb-4 block md:absolute top-0">
                     <button
                         className={`${listType === 'LAMELLAS' ? 'bg-white shadow-lg text-black' : ''} w-1/2 py-2 font-bold py-3 rounded-lg focus:outline-none`}
@@ -50,6 +61,7 @@ const CabinetList = ({history, setCabinet, search, setSearch}) => {
                             setOffset(0);
                         }}>
                         Gabinetes de Laminillas
+                        <span className="float-right mr-5">({getTotalItemsCount('Laminillas')})</span>
                     </button>
                     <button
                         className={`${listType === 'BLOCKS' ? 'bg-white shadow-lg text-black' : ''} w-1/2 py-2 font-bold py-3 rounded-lg focus:outline-none`}
@@ -58,30 +70,33 @@ const CabinetList = ({history, setCabinet, search, setSearch}) => {
                             setOffset(0);
                         }}>
                         Gabinetes de Bloques
+                        <span className="float-right mr-5">({getTotalItemsCount('Bloques')})</span>
                     </button>
                 </div>
                 <div className="flex justify-center mt-8"><RingLoader
                     size={100}
                     color={"#02b8ff"}
                     loading={loading}/></div>
-                <div className="w-full md:flex gap-3 px-10 md:px-0 relative">
+                <div className="w-full gap-3 px-10 md:px-0 relative">
                     <button
                         onClick={() => setOffset(offset - 3)}
                         disabled={offset === 0}
-                        className="py-4 px-6 bg-white shadow-lg absolute -ml-5 md:-ml-20 rounded-lg text-2xl focus:outline-none"
+                        className="py-4 px-6 bg-white shadow-lg absolute -ml-5 md:-ml-20 rounded-lg text-2xl focus:outline-none fixed"
                         style={{ top: "50%", marginTop: "-50px" }}>
                         <FontAwesomeIcon icon={faChevronLeft} />
                     </button>
                     <button
                         onClick={() => setOffset(offset + 3)}
                         disabled={(offset + 3) === list.length}
-                        className="py-4 px-6 bg-white shadow-lg absolute right-0 mr-5 md:-mr-20 rounded-lg text-2xl focus:outline-none"
+                        className="py-4 px-6 bg-white shadow-lg absolute right-0 mr-5 md:-mr-20 rounded-lg text-2xl focus:outline-none fixed"
                         style={{ top: "50%", marginTop: "-50px" }}>
                         <FontAwesomeIcon icon={faChevronRight} />
                     </button>
-                    {
-                        filteredItems().map((item, index) => <ListItem key={index} item={item} history={history} listType={listType} setCabinet={setCabinet} />)
-                    }
+                    <div className="flex flex-col md:flex-row justify-center w-full max-h-full gap-3">
+                        {
+                            filteredItems().map((item, index) => <ListItem key={index} item={item} history={history} listType={listType} setCabinet={setCabinet} />)
+                        }
+                    </div>
                 </div>
         </div>
     );
